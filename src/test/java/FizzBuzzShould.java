c8d290318bbce504892d418050dfec8b90d636f2
JesusArtiles <jesusartilescidoncha@gamil.com>
2020-10-02 15:09:19 +0100

    Test Class Refactor(remove unnecessary Tests)

--- src/test/java/FizzBuzzShould.java
+++ src/test/java/FizzBuzzShould.java
@@ -4,26 +4,6 @@ import FizzBuzz.*;
 
 public class FizzBuzzShould {
 
-    @Test
-    public void return_1_when_next_number_to_print_is_1(){
-        String result = new FizzBuzz().evaluateNumber(1);
-
-        assertThat(result).isEqualTo("1");
-    }
-
-    @Test
-    public void return_2_when_next_number_to_print_is_2(){
-        String result = new FizzBuzz().evaluateNumber(2);
-
-        assertThat(result).isEqualTo("2");
-    }
-
-    @Test
-    public void return_Fizz_when_next_number_to_print_is_3(){
-        String result = new FizzBuzz().evaluateNumber(3);
-
-        assertThat(result).isEqualTo("Fizz");
-    }
 
     @Test
     public void return_Fizz_when_next_number_to_print_is_divisible_by_3(){
@@ -36,16 +16,7 @@ public class FizzBuzzShould {
         assertThat(result3).isEqualTo("Fizz");
     }
 
-    @Test
-    public void return_normal_number_when_number_is_not_divisible_by_three(){
-        String result = new FizzBuzz().evaluateNumber(4);
-        String result2 = new FizzBuzz().evaluateNumber(7);
-        String result3 = new FizzBuzz().evaluateNumber(8);
 
-        assertThat(result).isEqualTo("4");
-        assertThat(result2).isEqualTo("7");
-        assertThat(result3).isEqualTo("8");
-    }
 
     @Test
     public void return_Buzz_when_next_number_to_print_is_divisible_by_5(){
@@ -56,6 +27,17 @@ public class FizzBuzzShould {
         assertThat(result).isEqualTo("Buzz");
         assertThat(result2).isEqualTo("Buzz");
         assertThat(result3).isEqualTo("Buzz");
+    }
+
+    @Test
+    public void return_normal_number_when_number_is_not_divisible_by_3_or_5(){
+        String result = new FizzBuzz().evaluateNumber(4);
+        String result2 = new FizzBuzz().evaluateNumber(7);
+        String result3 = new FizzBuzz().evaluateNumber(8);
+
+        assertThat(result).isEqualTo("4");
+        assertThat(result2).isEqualTo("7");
+        assertThat(result3).isEqualTo("8");
     }
 
     @Test
