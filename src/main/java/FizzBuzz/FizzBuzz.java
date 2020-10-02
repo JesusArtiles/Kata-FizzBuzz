2ff15800c8d4141325c3512a135af9240edb1eb0
JesusArtiles <jesusartilescidoncha@gamil.com>
2020-10-02 15:05:52 +0100

    Added main method that prints numbers from 1 to 100

--- src/main/java/FizzBuzz/FizzBuzz.java
+++ src/main/java/FizzBuzz/FizzBuzz.java
@@ -2,10 +2,17 @@ package FizzBuzz;
 
 public class FizzBuzz {
 
-    public String evaluateNumber(Integer number){
+    public static void main(String[] args) {
+        for (int i = 1;i < 101;i++){
+            System.out.println(evaluateNumber(i));
+        }
+    }
+
+    public static String evaluateNumber(Integer number){
         if(number%3 == 0 && number%5 == 0) return "FizzBuzz";
         else if(number%3 == 0) return "Fizz";
         else if(number%5 == 0) return "Buzz";
         else return number.toString();
     }
+
 }
